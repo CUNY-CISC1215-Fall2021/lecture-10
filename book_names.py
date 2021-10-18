8f3ade2f532763febd1e623a33bd3374eb072a01
Matt McNeill <mattm458@gmail.com>
2021-10-18 09:42:30 -0400

    comment update

--- book_names.py
+++ book_names.py
@@ -2,7 +2,7 @@ def name_find(text, names):
     """Find the first occurrence of a character in a book.
     text: The full text of a book. Should be a list of strings,
           each string representing a line of text in the book.
-    names: A list of characer names to search for.
+    names: A list of character names to search for.
 
     Returns a (name, line) tuple where "name" is the earliest-
     occurring character name, and "line" is the line number
